df5a05e6bab51492e889b5a7f235d501db235989
evabwiz <ebw2420@email.vccs.edu>
2024-02-25 14:09:53 -0500

    Home page, areas for crops and supplies

---
+++ programmers/PumpkinPatchApp/PumpkinPatchApp/Areas/Crops/CropsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PumpkinPatchApp.Areas.Crops
+{
+    public class CropsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Crops";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Crops_default",
+                "Crops/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
