cf83ac499ccf2e76ccfc65e965633ffee72b76cb
evabwiz <ebw2420@email.vccs.edu>
2024-02-27 20:53:20 -0500

    css color palettes

--- programmers/PumpkinPatchApp/PumpkinPatchApp/App_Start/BundleConfig.cs
+++ programmers/PumpkinPatchApp/PumpkinPatchApp/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace PumpkinPatchApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/custom.css"));
+                      "~/Content/custom.css",
+                      "~/Content/main.min.css"));
         }
     }
 }
