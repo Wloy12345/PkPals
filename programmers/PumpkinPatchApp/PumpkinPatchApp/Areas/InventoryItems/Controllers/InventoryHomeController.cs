7bb055c183448f94f672b884e5924aab58a4b659
Dave <Dave@LAPTOP-C8FTBHBS>
2024-03-26 18:43:09 -0400

    David 3/26 6:42pm

---
+++ programmers/PumpkinPatchApp/PumpkinPatchApp/Areas/InventoryItems/Controllers/InventoryHomeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PK_EF;
+
+namespace PumpkinPatchApp.Areas.InventoryItems.Controllers
+{
+    public class InventoryHomeController : Controller
+    {
+        private PKPalsEntities db = new PKPalsEntities();
+
+        // GET: InventoryItems/InventoryHome
+        public ActionResult Index()
+        {
+            var iNVENTORies = db.INVENTORies.Include(i => i.FARM).Include(i => i.SUPPLy);
+            return View(iNVENTORies.ToList());
+        }
+
+        // GET: InventoryItems/InventoryHome/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            INVENTORY iNVENTORY = db.INVENTORies.Find(id);
+            if (iNVENTORY == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iNVENTORY);
+        }
+
+        // GET: InventoryItems/InventoryHome/Create
+        public ActionResult Create()
+        {
+            ViewBag.FarmID = new SelectList(db.FARMs, "FarmID", "Name");
+            ViewBag.SuppliesID = new SelectList(db.SUPPLIES, "SuppliesID", "Name");
+            return View();
+        }
+
+        // POST: InventoryItems/InventoryHome/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "InventoryID,FarmID,SuppliesID,Quantity")] INVENTORY iNVENTORY)
+        {
+            if (ModelState.IsValid)
+            {
+                db.INVENTORies.Add(iNVENTORY);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FarmID = new SelectList(db.FARMs, "FarmID", "Name", iNVENTORY.FarmID);
+            ViewBag.SuppliesID = new SelectList(db.SUPPLIES, "SuppliesID", "Name", iNVENTORY.SuppliesID);
+            return View(iNVENTORY);
+        }
+
+        // GET: InventoryItems/InventoryHome/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            INVENTORY iNVENTORY = db.INVENTORies.Find(id);
+            if (iNVENTORY == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FarmID = new SelectList(db.FARMs, "FarmID", "Name", iNVENTORY.FarmID);
+            ViewBag.SuppliesID = new SelectList(db.SUPPLIES, "SuppliesID", "Name", iNVENTORY.SuppliesID);
+            return View(iNVENTORY);
+        }
+
+        // POST: InventoryItems/InventoryHome/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "InventoryID,FarmID,SuppliesID,Quantity")] INVENTORY iNVENTORY)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(iNVENTORY).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FarmID = new SelectList(db.FARMs, "FarmID", "Name", iNVENTORY.FarmID);
+            ViewBag.SuppliesID = new SelectList(db.SUPPLIES, "SuppliesID", "Name", iNVENTORY.SuppliesID);
+            return View(iNVENTORY);
+        }
+
+        // GET: InventoryItems/InventoryHome/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            INVENTORY iNVENTORY = db.INVENTORies.Find(id);
+            if (iNVENTORY == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iNVENTORY);
+        }
+
+        // POST: InventoryItems/InventoryHome/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            INVENTORY iNVENTORY = db.INVENTORies.Find(id);
+            db.INVENTORies.Remove(iNVENTORY);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ programmers/PumpkinPatchApp/PumpkinPatchApp/Areas/InventoryItems/InventoryItemsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PumpkinPatchApp.Areas.InventoryItems
+{
+    public class InventoryItemsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "InventoryItems";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "InventoryItems_default",
+                "InventoryItems/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
