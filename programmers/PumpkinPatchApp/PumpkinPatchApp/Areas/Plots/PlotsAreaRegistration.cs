6028186c180e222b752d43de7aaf80a1f74bc8a3
evabwiz <ebw2420@email.vccs.edu>
2024-04-06 19:34:16 -0400

    datatables finished!!!!!!!!

---
+++ programmers/PumpkinPatchApp/PumpkinPatchApp/Areas/Plots/PlotsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PumpkinPatchApp.Areas.Plots
+{
+    public class PlotsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Plots";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Plots_default",
+                "Plots/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ programmers/PumpkinPatchApp/PumpkinPatchApp/Areas/WaterStatusIndex/WaterStatusIndexAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PumpkinPatchApp.Areas.WaterStatusIndex
+{
+    public class WaterStatusIndexAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "WaterStatusIndex";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "WaterStatusIndex_default",
+                "WaterStatusIndex/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
