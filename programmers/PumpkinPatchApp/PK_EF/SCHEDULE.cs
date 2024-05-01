e9bc49460e177e7505436448efab96099623089e
Dave <Dave@LAPTOP-C8FTBHBS>
2024-05-01 00:53:56 -0400

    David 5/1

--- programmers/PumpkinPatchApp/PK_EF/SCHEDULE.cs
+++ programmers/PumpkinPatchApp/PK_EF/SCHEDULE.cs
@@ -19,7 +19,7 @@ namespace PK_EF
         public int EquipmentID { get; set; }
         public System.DateTime Time { get; set; }
         public string IsApproved { get; set; }
-        public Nullable<System.DateTime> EndTime { get; set; }
+        public System.DateTime EndTime { get; set; }
     
         public virtual EMPLOYEE EMPLOYEE { get; set; }
         public virtual EQUIPMENT EQUIPMENT { get; set; }
